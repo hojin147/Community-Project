a87bf857c6f9c9dbf89403c2f0570f7493585956
hojin147 <90072892+hojin147@users.noreply.github.com>
2021-09-21 17:43:33 +0900

    Community-Project

--- Community/src/main/java/com/bit/controller/CommentController.java
+++ Community/src/main/java/com/bit/controller/CommentController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +19,7 @@ import com.bit.service.CommentService;
 import com.bit.service.GalleryService;
 import com.bit.vo.CommentVO;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 
 //댓글 관련 컨트롤러
